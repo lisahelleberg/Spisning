8b5d189c0008e620a57fe87308e2905ceb5d8d31
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2016-12-11 21:19:11 +0100

    Har lavet knap på forsiden til tilmeldingssiden

---
+++ Spisning/Model/Tilmelding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spisning
+{
+    public class Tilmelding
+    {
+        public int Husnummer { get; set; }
+        public double Voksne { get; set; }
+        public double Teens { get; set; }
+        public double Boern { get; set; }
+        public double Baby { get; set; }
+    }
+}
